66e3ba692deb58f8ea37e9c60185ea2b1a1ce92e
Gareth Sanashee <gareth.sanashee@appsinthesky.com>
2020-02-01 14:19:28 +0000

    new branch

---
+++ app/src/main/java/com/thetrainingplan/ToolbarFragment.kt
@@ -0,0 +1,37 @@
+package com.thetrainingplan
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.thetrainingplan.databinding.FragmentToolbarBinding
+import com.thetrainingplan.viewmodels.MainViewModel
+
+
+class ToolbarFragment : Fragment() {
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        /*// Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_toolbar, container, false)*/
+
+        viewModel = ViewModelProviders.of(activity!!).get(MainViewModel::class.java)
+
+        val binding: FragmentToolbarBinding = FragmentToolbarBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+
+        viewModel.startProfileActivityEvent.observe(this, Observer {
+            val intent = Intent(activity, ActivityProfile::class.java)
+            startActivity(intent)
+        })
+
+        return binding.root
+    }
+
+}
