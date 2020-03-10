6890c276c0ad1975fefb17b2ac8c8e0315566c69
Gareth Sanashee <gareth.sanashee@appsinthesky.com>
2020-03-10 19:49:42 +0000

    UI for goals with switch

--- app/src/main/java/com/thetrainingplan/viewmodels/GoalsViewModel.kt
+++ app/src/main/java/com/thetrainingplan/viewmodels/GoalsViewModel.kt
@@ -2,8 +2,20 @@ package com.thetrainingplan.viewmodels
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
 
 class GoalsViewModel(application : Application) : AndroidViewModel(application)  {
 
+    var isChecked = MutableLiveData<Boolean>().apply { value = false }
 
+    fun switchPressed(){
+        isChecked.value?.let {
+            if(it){
+                isChecked.value = false
+            }
+            else{
+                isChecked.value = true
+            }
+        }
+    }
 }
