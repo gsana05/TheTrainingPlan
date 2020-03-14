13df2cfe55ae7d843572ae4ced74243c56a02a1d
Gareth Sanashee <gareth.sanashee@appsinthesky.com>
2020-03-14 11:09:46 +0000

    change editText and textview to use mutable live data

--- app/src/main/java/com/thetrainingplan/GoalsActivity.kt
+++ app/src/main/java/com/thetrainingplan/GoalsActivity.kt
@@ -4,43 +4,27 @@ import android.app.DatePickerDialog
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
-import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.inputmethod.EditorInfo
-import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.TextView
 import androidx.annotation.LayoutRes
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProviders
 import com.thetrainingplan.databinding.ActivityGoalsBinding
-import com.thetrainingplan.databinding.ActivityMainBinding
 import com.thetrainingplan.models.GoalTypeSpinner
 import com.thetrainingplan.viewmodels.GoalsViewModel
-import com.thetrainingplan.viewmodels.MainViewModel
 import kotlinx.android.synthetic.main.activity_goals.*
-import org.jetbrains.anko.alert
-import org.jetbrains.anko.okButton
 import android.view.MotionEvent
-import androidx.core.app.ComponentActivity.ExtraData
-import androidx.core.content.ContextCompat.getSystemService
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
 import java.text.DateFormat
 import java.text.SimpleDateFormat
 import java.util.*
-import java.util.concurrent.TimeUnit
 import kotlin.collections.ArrayList
-
 
 class GoalsActivity : AppCompatActivity() {
 
     private lateinit var viewModel: GoalsViewModel
-    private var selectedControlView : View? = null
-    private var mSwitchFilterStyle = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -66,22 +50,11 @@ class GoalsActivity : AppCompatActivity() {
                 c.set(Calendar.MONTH, monthOfYear)
                 c.set(Calendar.DAY_OF_MONTH, dayOfMonth)
 
-                val timeInMillies = c.timeInMillis
-
-                val textView = goals_spinner_goal_date_deadline_input as TextView
-                textView.text = SimpleDateFormat.getDateInstance(DateFormat.LONG).format(c.time)
-
-                /*val millionSeconds = timeInMillies - Calendar.getInstance().timeInMillis
-                goals_number_of_days.text = TimeUnit.MILLISECONDS.toDays(millionSeconds).toString() + "days"*/
-
-                val cal = Calendar.getInstance()
-                //val SimpleDateFormat = SimpleDateFormat("EEE MMM dd hh:mm:ss 'GMT'Z yyyy")
-                val now = cal.time
-                //val i = SimpleDateFormat.format(cal.time)
+                viewModel.dateGoalDeadlineInMillie.value = c.timeInMillis
+                viewModel.dateGoalDeadline.value = SimpleDateFormat.getDateInstance(DateFormat.LONG).format(c.time)
 
 
-                printDifference(now, c.time)
-
+                viewModel.printDifference(Calendar.getInstance().time, c.time)
 
             }, year, month, day)
 
@@ -106,65 +79,7 @@ class GoalsActivity : AppCompatActivity() {
         }
         goals_spinner_goal_type.adapter = spinnerAdapterMinDealSize
 
-        goals_spinner_goal_type.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onNothingSelected(parent: AdapterView<*>?) {
 
-            }
-
-            override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
-                parent.let {
-
-                    mSwitchFilterStyle = true
-                    selectedControlView = goals_spinner_goal_type
-                    updateUI()
-                }
-            }
-        }
-
-        updateUI()
-
-    }
-
-    //1 minute = 60 seconds
-//1 hour = 60 x 60 = 3600
-//1 day = 3600 x 24 = 86400
-    fun printDifference(startDate : Date, endDate : Date) {
-    //milliseconds
-    var different = endDate.time - startDate.time
-
-    val secondsInMilli = 1000
-    val minutesInMilli = secondsInMilli * 60
-    val hoursInMilli = minutesInMilli * 60
-    val daysInMilli = hoursInMilli * 24
-
-    val elapsedDays = different / daysInMilli
-        different %= daysInMilli
-
-    val elapsedHours = different / hoursInMilli
-        different %= hoursInMilli
-
-    val elapsedMinutes = different / minutesInMilli
-        different %= minutesInMilli
-
-    val elapsedSeconds = different / secondsInMilli
-
-    System.out.printf(
-        "%d days, %d hours, %d minutes, %d seconds%n",
-        elapsedDays, elapsedHours, elapsedMinutes, elapsedSeconds)
-
-
-        System.out.printf(
-            "%d days, %d hours, %d minutes, %d seconds%n",
-            elapsedDays, elapsedHours, elapsedMinutes, elapsedSeconds)
-
-        goals_number_of_days.text = getString(R.string.timeUntilGoalDeadline, elapsedDays, elapsedHours)
-}
-
-    fun updateUI(){
-        // min deal size
-        /*goals_spinner_goal_type.setBackgroundResource(if( selectedControlView == goals_spinner_goal_type && mSwitchFilterStyle){R.drawable.gradient_spinner_highlight} else {R.drawable.gradient_spinner})
-        goals_spinner_goal_type.setColorFilter(if( selectedControlView == goals_spinner_goal_type  && mSwitchFilterStyle){colGreen} else {colWhite})*/
-        goals_spinner_goal_type_arrow.rotation = if( selectedControlView == goals_spinner_goal_type  && mSwitchFilterStyle){0.0f} else {180.0f}
     }
 
     // custom spinner that takes null values
--- app/src/main/java/com/thetrainingplan/viewmodels/GoalsViewModel.kt
+++ app/src/main/java/com/thetrainingplan/viewmodels/GoalsViewModel.kt
@@ -3,8 +3,6 @@ package com.thetrainingplan.viewmodels
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
-import android.widget.TextView
 import java.text.DateFormat
 import java.text.SimpleDateFormat
 import java.util.*
@@ -14,6 +12,12 @@ class GoalsViewModel(application : Application) : AndroidViewModel(application) 
 
     var isChecked = MutableLiveData<Boolean>().apply { value = false }
     var dateOfGoalSet = MutableLiveData<String>()
+    var dateOfGoalSetInMillie = MutableLiveData<Long>()
+    var goalSet = MutableLiveData<String>()
+    var dateGoalDeadline = MutableLiveData<String>()
+    var dateGoalDeadlineInMillie = MutableLiveData<Long>()
+    var numberOfDaysToGoal = MutableLiveData<String>()
+    var numberOfDaysToGoalInMillie = MutableLiveData<Long>()
 
     init {
         getCurrentDate()
@@ -26,18 +30,38 @@ class GoalsViewModel(application : Application) : AndroidViewModel(application) 
         c.get(Calendar.MONTH)
         c.get(Calendar.DAY_OF_MONTH)
 
+        dateOfGoalSetInMillie.value = c.timeInMillis
         dateOfGoalSet.value = SimpleDateFormat.getDateInstance(DateFormat.LONG).format(c.time)
     }
 
     fun switchPressed(){
         isChecked.value?.let {
-            if(it){
-                isChecked.value = false
-            }
-            else{
-                isChecked.value = true
-            }
+            isChecked.value = !it
         }
+    }
+
+    fun printDifference(startDate : Date, endDate : Date) {
+        //milliseconds
+        var different = endDate.time - startDate.time
+        numberOfDaysToGoalInMillie.value = different
+
+        val secondsInMilli = 1000
+        val minutesInMilli = secondsInMilli * 60
+        val hoursInMilli = minutesInMilli * 60
+        val daysInMilli = hoursInMilli * 24
+
+        val elapsedDays = different / daysInMilli
+        different %= daysInMilli
+
+        val elapsedHours = different / hoursInMilli
+        different %= hoursInMilli
+
+        //val elapsedMinutes = different / minutesInMilli
+        different %= minutesInMilli
+
+        //val elapsedSeconds = different / secondsInMilli
+
+        numberOfDaysToGoal.value = "DAYS: $elapsedDays and HOURS $elapsedHours"
     }
 
 }
