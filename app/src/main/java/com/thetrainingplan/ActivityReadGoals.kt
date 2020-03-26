97109acf4e6724cca19d0856647e714e51c45ad2
GarethSanashee <gareth.sanashee@appsinthesky.com>
2020-03-26 23:47:21 +0000

    added app tab layout with fragment

--- app/src/main/java/com/thetrainingplan/ActivityReadGoals.kt
+++ app/src/main/java/com/thetrainingplan/ActivityReadGoals.kt
@@ -1,12 +1,95 @@
 package com.thetrainingplan
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.google.android.material.tabs.TabLayout
+import kotlinx.android.synthetic.main.activity_read_goals.*
+import kotlinx.android.synthetic.main.fragment_fragment_read_goals.view.*
 
+@Suppress("DEPRECATION")
 class ActivityReadGoals : AppCompatActivity() {
+
+    private var mSectionsPagerAdapter: SectionsPagerAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_read_goals)
+
+        mSectionsPagerAdapter = SectionsPagerAdapter(supportFragmentManager)
+
+        // Set up the ViewPager with the sections adapter.
+        view_pager.adapter = mSectionsPagerAdapter
+
+        view_pager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabs))
+        tabs.addOnTabSelectedListener(TabLayout.ViewPagerOnTabSelectedListener(view_pager))
+
+
+    }
+
+    inner class SectionsPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+
+        override fun getItem(position: Int): Fragment {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+            return PlaceholderFragment.newInstance(position + 1)
+        }
+
+        override fun getCount(): Int {
+            // Show 3 total pages.
+            return 3
+        }
+    }
+}
+
+class PlaceholderFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val rootView = inflater.inflate(R.layout.fragment_fragment_read_goals, container, false)
+
+        if(arguments?.getInt(ARG_SECTION_NUMBER) == 1){
+            val openGoals = rootView.context.resources.getString(R.string.open_goals)
+            rootView.fragment_read_goals_text_view.text = openGoals
+        }
+
+        if(arguments?.getInt(ARG_SECTION_NUMBER) == 2){
+            val completedGoals = rootView.context.resources.getString(R.string.completed_goals)
+            rootView.fragment_read_goals_text_view.text = completedGoals
+        }
+
+        if(arguments?.getInt(ARG_SECTION_NUMBER) == 3){
+            val deletedGoals = rootView.context.resources.getString(R.string.deleted_goals)
+            rootView.fragment_read_goals_text_view.text = deletedGoals
+        }
+
+        return rootView
+    }
+
+    companion object {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private const val ARG_SECTION_NUMBER = "section_number"
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        fun newInstance(sectionNumber: Int): PlaceholderFragment {
+            val fragment = PlaceholderFragment()
+            val args = Bundle()
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber)
+            fragment.arguments = args
+            return fragment
+        }
     }
 }
