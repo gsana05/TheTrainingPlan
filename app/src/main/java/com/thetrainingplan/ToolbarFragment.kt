6921f67afeaa3dcb8144089285c9d9fb8d79596c
GarethSanashee <gareth.sanashee@appsinthesky.com>
2020-04-13 11:56:24 +0100

    clear up warnings

--- app/src/main/java/com/thetrainingplan/ToolbarFragment.kt
+++ app/src/main/java/com/thetrainingplan/ToolbarFragment.kt
@@ -1,12 +1,10 @@
 package com.thetrainingplan
 
-import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.Observer
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import com.thetrainingplan.databinding.FragmentToolbarBinding
 import com.thetrainingplan.viewmodels.MainViewModel
--- app/src/main/java/com/thetrainingplan/ToolbarProfileFragment.kt
+++ app/src/main/java/com/thetrainingplan/ToolbarProfileFragment.kt
@@ -2,12 +2,11 @@ package com.thetrainingplan
 
 import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
-import androidx.core.content.ContextCompat.getSystemService
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.thetrainingplan.databinding.FragmentToolbarProfileBinding
